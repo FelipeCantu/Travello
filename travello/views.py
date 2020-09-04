10fba3b548ba62b9ce7bac242c753b996c46bb78
Felipe Cantu <36748934+ABACABB00@users.noreply.github.com>
2020-09-04 03:06:50 -0500

    destinations and offers

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from .models import Destinations
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    des_1 = Destinations()
+    des_1.name = 'United States'
+    des_1.desc = 'Major Atlantic Coast cities are New York'
+    des_1.img = 'destination_1.jpg'
+    des_1.price = 850
+    des_1.offer = False
+
+    des_2 = Destinations()
+    des_2.name = 'Switzerland'
+    des_2.desc = 'mountainous Central European country'
+    des_2.img = 'destination_2.jpg'
+    des_2.price = 650
+    des_2.offer = True
+
+    des_3 = Destinations()
+    des_3.name = 'Mars'
+    des_3.desc = 'fourth planet from the Sun and the second-smallest planet in the Solar System'
+    des_3.img = 'destination_3.jpg'
+    des_3.price = 1670
+    des_3.offer = False
+
+    dest = [des_1, des_2, des_3]
+
+    return render(request, 'index.html', {'dest' : dest})
+
+
+
