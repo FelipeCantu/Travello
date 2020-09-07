f333f0e002c7f81d2cb29af842244f229638a1a7
Felipe Cantu <36748934+ABACABB00@users.noreply.github.com>
2020-09-07 01:50:13 -0500

    register, auth

--- travello/models.py
+++ travello/models.py
@@ -6,5 +6,5 @@ class Destinations(models.Model):
     name = models.CharField(max_length=100)
     img = models.ImageField(upload_to='pic')
     desc = models.TextField()
-    price = models.ImageField()
+    price = models.IntegerField()
     offer = models.BooleanField(default=False)
--- travello/views.py
+++ travello/views.py
@@ -5,28 +5,7 @@ from .models import Destinations
 
 def index(request):
 
-    des_1 = Destinations()
-    des_1.name = 'United States'
-    des_1.desc = 'Major Atlantic Coast cities are New York'
-    des_1.img = 'destination_1.jpg'
-    des_1.price = 850
-    des_1.offer = False
-
-    des_2 = Destinations()
-    des_2.name = 'Switzerland'
-    des_2.desc = 'mountainous Central European country'
-    des_2.img = 'destination_2.jpg'
-    des_2.price = 650
-    des_2.offer = True
-
-    des_3 = Destinations()
-    des_3.name = 'Mars'
-    des_3.desc = 'fourth planet from the Sun and the second-smallest planet in the Solar System'
-    des_3.img = 'destination_3.jpg'
-    des_3.price = 1670
-    des_3.offer = False
-
-    dest = [des_1, des_2, des_3]
+    dest = Destinations.objects.all()
 
     return render(request, 'index.html', {'dest' : dest})
 
